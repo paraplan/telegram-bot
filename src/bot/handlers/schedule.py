7ae56d98fbafec219df72d6b3365d60c1af514f0
VodoGamer <vodogamer@yandex.ru>
2025-01-27 15:17:02 +0300

    feat: add date-specific schedule command

--- src/bot/handlers/schedule.py
+++ src/bot/handlers/schedule.py
@@ -1,9 +1,9 @@
 import datetime
 
-from telegrinder import Dispatch, Message
-from telegrinder.rules import Command
+from telegrinder import MESSAGE_FROM_USER, Dispatch, Message, MessageReplyHandler
+from telegrinder.rules import Command, Regex
 
-from src.bot.client import formatter
+from src.bot.client import formatter, wm
 from src.bot.utils.schedule import render_schedule_for_date
 from src.database import RepositoryFactory
 from src.database.models import User
@@ -47,5 +47,21 @@ async def handle_monday(message: Message, user: User, repository: RepositoryFact
 async def handle_week(message: Message, user: User, repository: RepositoryFactory):
     text, keyboard = await render_schedule_for_date(
         repository, message.date, user.group, user.settings.subgroup, is_week=True
+    )
+    await message.answer(text, parse_mode=formatter.PARSE_MODE, reply_markup=keyboard)
+
+
+@dp.message(Command("date"))
+async def handle_date(message: Message, user: User, repository: RepositoryFactory):
+    await message.answer("Напишите дату в формате DD.MM.YYYY")
+    message, _ = await wm.wait(
+        MESSAGE_FROM_USER,
+        message.from_user.id,
+        release=Regex(r"\d{2}\.\d{2}\.\d{4}"),
+        on_miss=MessageReplyHandler("Вы ввели некорректную дату", as_reply=True),
+    )
+    date = datetime.datetime.strptime(message.text.unwrap(), "%d.%m.%Y")
+    text, keyboard = await render_schedule_for_date(
+        repository, date, user.group, user.settings.subgroup, is_week=True
     )
     await message.answer(text, parse_mode=formatter.PARSE_MODE, reply_markup=keyboard)
