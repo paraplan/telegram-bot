54d6067e53406d795b6f4f6b4dfe6589f6650764
VodoGamer <vodoplayyt1@gmail.com>
2025-09-17 19:02:00 +0300

    chore: update dependencies

--- src/bot/client.py
+++ src/bot/client.py
@@ -1,12 +1,12 @@
 import locale
 
 from telegrinder import API, Dispatch, HTMLFormatter, Telegrinder, Token, WaiterMachine
-from telegrinder.modules import logger
+from telegrinder.modules import setup_logger
 
 from src.bot.utils.middlewares import AllowedUsersMiddleware
 from src.env import BOT_TOKEN, LOGGER_LEVEL
 
-logger.set_level(LOGGER_LEVEL)
+setup_logger(format=LOGGER_LEVEL)
 locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 
 api = API(token=Token(BOT_TOKEN))
@@ -16,4 +16,4 @@ wm = WaiterMachine(dp)
 
 formatter = HTMLFormatter
 
-bot.on.message.register_middleware()(cls=AllowedUsersMiddleware)
+bot.on.message.register_middleware(AllowedUsersMiddleware)
